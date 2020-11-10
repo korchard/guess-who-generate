c641a76976266280a8491c984840220a53b42bdf
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2020-11-09 20:20:30 -0600

    updated the styling

--- data.js
+++ data.js
@@ -24,7 +24,43 @@ var people = [
     githubUsername: 'cassandradanger'
   },
   {
-    name: 'Chris',
+    name: 'Chris B',
     githubUsername: 'christopher-black'
+  },
+  {
+    name: 'Jim B',
+    githubUsername: 'jbird55044'
+  },
+  {
+    name: 'Brady B',
+    githubUsername: 'bradybaker'
+  },
+  {
+    name: 'Jordan N',
+    githubUsername: 'jordanNewberry21'
+  },
+  {
+    name: 'Julian B',
+    githubUsername: 'julianbooher'
+  },
+  {
+    name: 'Yer T',
+    githubUsername: 'yyerthao'
+  },
+  {
+    name: 'John M',
+    githubUsername: 'jmicko'
+  },
+  {
+    name: 'James P',
+    githubUsername: 'jposeyjr'
+  },
+  {
+    name: 'Lara L',
+    githubUsername: 'laramaurine'
+  },
+  {
+    name: 'Kimberly O',
+    githubUsername: 'korchard'
   },
 ];
