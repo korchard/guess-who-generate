5585d9f03b2469d78eb2ec27a551fa609b7d96ac
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2020-11-09 23:48:02 -0600

    used the Fisher-Yates algorithm to shuffle the display of images when the game begins

--- client.js
+++ client.js
@@ -44,22 +44,11 @@ function keepPlaying() {
     alert(`Choose ${people[randomIndex].name}!`);
 } // end keepPlaying function
 
+//THIS IS THE FISHER-YATES ALGORITHM
 function shuffle(people) {
     for (let i = people.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
-        [people[i], people[j]] = [people[j], people[i]];
-    }
-}
-
-/*function shuffle(people) {
-    for (let i = people.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
-  
-      // swap elements array[i] and array[j]
-      // we use "destructuring assignment" syntax to achieve that
-      // you'll find more details about that syntax in later chapters
-      // same can be written as:
-      // let t = array[i]; array[i] = array[j]; array[j] = t
+      // swap elements array[i] and array[j] using 'destructuring assignment'
       [people[i], people[j]] = [people[j], people[i]];
-    }
-  }*/
+    } // end for loop
+  } // end shuffle function
